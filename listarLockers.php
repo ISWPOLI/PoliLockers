aa345b3206fde07872aa56e7ecb8a4ee8830eea8
FranciscoSegura07 <lfsegura@poligran.edu.co>
2017-05-12 02:10:01 -0500

    Commit #5
    Actualización modulo Administrador 
    #15 
    #16 
    #21 
    #22 
    #42

--- listarLockers.php
+++ listarLockers.php
@@ -12,24 +12,13 @@
         <title>NUEVO</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximun-scale=1, minimun-scale=1">
-        <link rel="stylesheet" href="css/fontello.css">
-        <link rel="stylesheet" href="css/estilosAdm.css">
-        <link rel="stylesheet" href="css/estiloMenu.css">
+        <link rel="stylesheet" href="css/estiloListas.css">
     </head>
     <body>
        <header>
-           <div class="contenedor">
-                <h1 class="icon-admin"> Lista de lockers</h1>
-                <input type="checkbox" id="menu-bar">
-                <label class="icon-menu "for="menu-bar"></label>
-                <nav class="menu">
-                    <a class="icon-profesor" href="adminUser.html"> Administrar Usuarios</a>
-                    <a class="icon-locker" href="adminLocker.html"> Administrar Lockers</a>
-                    <a class="icon-salir" href="index.html"> Salir</a>
-                </nav>
-            </div>
             <section>
             <table border="1">
+               <h1>LISTADO DE LOCKERS</h1>
                 <tr>
                     <td>ID</td><td>BLOQUE</td><td>ESTADO</td><td>COORDINADOR</td><td>PROFESOR</td>
                 </tr>
@@ -47,6 +36,5 @@
             </table>
         </section>
        </header>
-        
     </body>
 </html>
