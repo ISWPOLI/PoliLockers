6234306428a3701928e68b4233f663990f47e0a6
FranciscoSegura07 <lfsegura@poligran.edu.co>
2017-05-25 13:44:01 -0500

    Listas Responsive #68
    Se hacen los cambios para que las ventanas de listar usuarios y listar lockers sean responsive.

--- listarLockers.php
+++ listarLockers.php
@@ -1,35 +1,48 @@
 <?php
     $conexion=mysqli_connect("localhost", "root", "", "bdlockers");
-    
     $consulta="SELECT * FROM lockers";
     $registros=mysqli_query($conexion, $consulta);
-        
+    $registros1=mysqli_query($conexion, $consulta);  
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>NUEVO</title>
+        <title>POLILOCKERS</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximun-scale=1, minimun-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/estiloListas.css">
     </head>
     <body>
        <header>
-            <section>
-            <table border="1">
-               <h1>LISTADO DE LOCKERS</h1>
+           <h1>LISTADO DE LOCKERS</h1>
+           <HR align="CENTER" size="2" width="75%" color="white" noshade>
+            <section class="container">
+            <div id="f_lockers">
+               <?php
+                    while($row = $registros1->fetch_array(MYSQLI_BOTH)){
+                        echo '  <form>
+                                    <label class="tit">Id :&nbsp;</label><label class="res">'.$row['id'].'</label><br>
+                                    <label class="tit">Bloque :&nbsp;</label><label class="res">'.$row['bloque'].'</label><br>
+                                    <label class="tit">Ubicacion :&nbsp;</label><label class="res">'.$row['ubicacion'].'</label><br>
+                                    <label class="tit">Descripcion :&nbsp;</label><label class="res">'.$row['descripcion'].'</label><br>
+                                </form>';
+                    }
+                ?>
+            </div>
+            <table class="table table-striped tabla" id="t_lockers" border="1">
+               
                 <tr>
-                    <td>ID</td><td>BLOQUE</td><td>ESTADO</td><td>COORDINADOR</td><td>PROFESOR</td>
+                    <td><strong>ID</strong></td><td><strong>BLOQUE</strong></td><td><strong>UBICACION</strong></td><td><strong>DESCRIPCION</strong></td>
                 </tr>
                 <?php
                 while($row = $registros->fetch_array(MYSQLI_BOTH)){
                     echo    '<tr>
                             <td>'.$row['id'].'</td>
                             <td>'.$row['bloque'].'</td>
-                            <td>'.$row['estado'].'</td>
-                            <td>'.$row['coordinador'].'</td>
-                            <td>'.$row['profesor'].'</td>
+                            <td>'.$row['ubicacion'].'</td>
+                            <td>'.$row['descripcion'].'</td>
+                            
                             </tr>';
                 }
                 ?>
--- listarUsuarios.php
+++ listarUsuarios.php
@@ -2,28 +2,42 @@
     $conexion=mysqli_connect("localhost", "root", "", "bdlockers");
     $consulta="SELECT * FROM usuarios";
     $registros=mysqli_query($conexion, $consulta);      
+    $registros1=mysqli_query($conexion, $consulta);  
 ?>
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>USUARIOS</title>
+        <title>POLILOCKERS</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximun-scale=1, minimun-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/estiloListas.css">
     </head>
     <body>
        <header>
             <h1>LISTADO DE USUARIOS</h1>
-            <section>
-            <table border="1">
+            <HR align="CENTER" size="2" width="75%" color="white" noshade>
+            <section class="container">
+            <div id="f_users">
+               <?php
+                    while($row = $registros1->fetch_array(MYSQLI_BOTH)){
+                        echo '  <form>
+                                    <label class="tit">Usuario :&nbsp;</label><label class="res">'.$row['usuario'].'</label><br>
+                                    <label class="tit">Nombre :&nbsp;</label><label class="res">'.$row['nombre'].'</label><br>
+                                    <label class="tit">Rol :&nbsp;</label><label class="res">'.$row['rol'].'</label><br>
+                                    <label class="tit">Correo :&nbsp;</label><label class="res">'.$row['correo'].'</label><br>
+                                </form>';
+                    }
+                ?>
+            </div>
+            <table class="table table-striped tabla" id="t_users" border="1">
                 <tr>
-                    <td>USUARIO</td><td>CONTRASEÑA</td><td>NOMBRE</td><td>ROL</td><td>CORREO</td>
+                    <td><strong>USUARIO</strong></td><td><strong>NOMBRE</strong></td><td><strong>ROL</strong></td><td><strong>CORREO</strong></td>
                 </tr>
                 <?php
                     while($row = $registros->fetch_array(MYSQLI_BOTH)){
                         echo    '<tr>
                                 <td>'.$row['usuario'].'</td>
-                                <td>'.$row['clave'].'</td>
                                 <td>'.$row['nombre'].'</td>
                                 <td>'.$row['rol'].'</td>
                                 <td>'.$row['correo'].'</td>
@@ -33,5 +47,7 @@
             </table>
         </section>
        </header>
+       <script src="js/jquery.js"></script>
+       <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
